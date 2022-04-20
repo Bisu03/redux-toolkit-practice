7cc4f6c2eef6691721571a771e063ac390bf616e
unknown <berabiswanath338@gmail.com>
2022-04-20 21:00:15 +0530

    fix the fetching issue

--- src/store/productSlice.js
+++ src/store/productSlice.js
@@ -29,22 +29,17 @@ const productSlice = createSlice({
 export const { setProducts,setStatus } = productSlice.actions
 export default productSlice.reducer
 
-// export function fetchProduct() {
-//     return async function fetchProductThunk(dispatch, getState) {
-//         dispatch(setStatus(STATUSES.LODING))
-//         try {
-//             const res = await fetch('https://fakestoreapi.com/products')
-//             const data = await res.json()
-//             dispatch(setProducts(data))
-//             dispatch(setStatus(STATUSES.IDLE))
-//         } catch (error) {
-//             console.log(error);
-//             dispatch(setStatus(STATUSES.ERROR))
-//         }
-//     }
-// }
-
-export const fetchProduct = createAsyncThunk('product/fetch',async()=>{
-    const res = await fetch('https://fakestoreapi.com/products')
-     return await res.json()
-})
+export function fetchProduct() {
+    return async function fetchProductThunk(dispatch, getState) {
+        dispatch(setStatus(STATUSES.LODING))
+        try {
+            const res = await fetch('https://fakestoreapi.com/products')
+            const data = await res.json()
+            dispatch(setProducts(data))
+            dispatch(setStatus(STATUSES.IDLE))
+        } catch (error) {
+            console.log(error);
+            dispatch(setStatus(STATUSES.ERROR))
+        }
+    }
+}
